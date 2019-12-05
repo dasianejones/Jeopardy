cb8b422c5a75c14ea1548891efb14a55e9392262
Dasiane Jones <dasianejones@gmail.com>
2019-12-05 11:55:19 -0500

    updated class names in html

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+let card1Front = document.getElementsByClassName("front-cell")
+
+
+
+
+
+
 let gameQuestions = [{
 
     questionText: "This eight-day Jewish celebration commemorates the second century rededication of the Second Temple in Jerusalem where according to legend Jews had risen up against theirGreek-Syrian oppressors in the Maccabean Revolt.", 
     answerChoices: {
       correct: "Hannukah",
       wrong: ["Yule", "Fiesta of Our Lady of Guadalupe", "Saint Nicholas Day "]
-    }
-
+    } 
+ 
 }];
   
