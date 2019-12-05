f8f8184eb961f540599f386befe6c4e5e742ab6f
Dasiane Jones <dasianejones@gmail.com>
2019-12-05 09:55:32 -0500

    added new div class for back of card

---
+++ script.js
@@ -0,0 +1,10 @@
+let gameQuestions = [{
+
+    questionText: "This eight-day Jewish celebration commemorates the second century rededication of the Second Temple in Jerusalem where according to legend Jews had risen up against theirGreek-Syrian oppressors in the Maccabean Revolt.", 
+    answerChoices: {
+      correct: "Hannukah",
+      wrong: ["Yule", "Fiesta of Our Lady of Guadalupe", "Saint Nicholas Day "]
+    }
+
+}];
+  
