2dbe057f7f808a763462d82aee5a56d86ffda173
Dasiane Jones <dasianejones@gmail.com>
2019-12-06 13:41:38 -0500

    1st half of js content added

--- script.js
+++ script.js
@@ -1,17 +1,336 @@
-let card1Front = document.getElementsByClassName("front-cell")
-
-
-
-
-
-
 let gameQuestions = [{
-
+  
     questionText: "This eight-day Jewish celebration commemorates the second century rededication of the Second Temple in Jerusalem where according to legend Jews had risen up against theirGreek-Syrian oppressors in the Maccabean Revolt.", 
     answerChoices: {
       correct: "Hannukah",
       wrong: ["Yule", "Fiesta of Our Lady of Guadalupe", "Saint Nicholas Day "]
-    } 
- 
-}];
+
+    }, 
+    questionText: "This holiday is held in remembrance of the birth of of Jesus Christ, who Christians believe is the Son of God. The name comes from the Mass of Christ (or Jesus).",
+     answerChoices: {
+       correct: "Christmas",
+       wrong: ["Boxing Day", "St. Lucia Day", "Omisoka"]
+
+     },
+     questionText: "This African-American celebration of life runs from 26 December to 1 January. It was first introduced by Dr. Maulana Karenga in 1966 to the United States as a ritual to welcome the first harvests to the home.",
+     answerChoices: {
+      correct: "Kwanzaa",
+      wrong: ["Saturnalia", "Hanukkah", "Saint Nicholas Day"]
+
+    },
   
+    questionText: "This holiday is a popular Catholic feast that celebrates the belief that a man encountered the Virgin Mary, Mexico's patron saint, in Mexico City on December 9 and 12, 1531.",
+     answerChoices: {
+      correct: "Fiesta of Our Lady of Guadalupe",
+      wrong: ["Christmas", "Yule", "St. Lucia Day"]
+
+    },
+    questionText: "This holiday is the second-most important day in Japanese tradition because it is the final day of the old year and the eve of New Year's Day, which is the most important day of the year.",
+     answerChoices: {
+      correct: "Omisoka",
+      wrong: ["New Years", "Boxing Day", "Hanukkah"]
+
+    },
+    questionText: "In 2012, this gaming system was the first eight-generation video game console and the first Nintendo console to support HD graphics. It came with a GamePad controller, allowing users to play games without a TV display.",
+     answerChoices: {
+      correct: "Wii-U",
+      wrong: ["Xbox 360", "PS4", "Gameboy"]
+
+    },
+    questionText: "In 2010, this device made history as a new kind of portable device and the first successful tablet on the market, selling over 15 million items.",
+     answerChoices: {
+      correct: "iPad",
+      wrong: ["Galaxy Tablet", "Laptop", "Bluetooth Earphones"]
+
+    },
+    questionText: "In 1999, this Japanese cartoon franchise had children and ADULTS going crazy over its card game, Nintendo Game Boy game, and show.",
+     answerChoices: {
+      correct: "Pokemon",
+      wrong: ["Yugi-Oh", "Naruto", "Inuyasha"]
+
+    },
+    questionText: "In 1997, this must-have handheld digital pets released by Bandai became the biggest toy fads of the 1990s and early 2000s, with over 76 million being sold worldwide as of 2010.",
+     answerChoices: {
+      correct: "Tamagotchi",
+      wrong: ["Nintendo Game Boy", "Tablet", "Cell Phone"]
+
+    },
+    questionText: "In 1980, this 3-D combination puzzle, invented by Hungarian sculptor and professor of architecture Erno Rubik, is still considered to be the best selling toy.",
+     answerChoices: {
+      correct: "Rubiks Cube",
+      wrong: ["Solitaire", "Tetris", "Monopoly"]
+
+    },
+    questionText: "",
+     answerChoices: {
+      correct: "",
+      wrong: ["", "", ""]
+
+    },
+    questionText: "",
+     answerChoices: {
+      correct: "",
+      wrong: ["", "", ""]
+
+    },
+    questionText: "",
+     answerChoices: {
+      correct: "",
+      wrong: ["", "", ""]
+
+    },
+    questionText: "",
+     answerChoices: {
+      correct: "",
+      wrong: ["", "", ""]
+
+    },
+    questionText: "",
+     answerChoices: {
+      correct: "",
+      wrong: ["", "", ""]
+
+    },
+    questionText: "",
+     answerChoices: {
+      correct: "",
+      wrong: ["", "", ""]
+
+    },
+    questionText: "",
+     answerChoices: {
+      correct: "",
+      wrong: ["", "", ""]
+
+    },
+    questionText: "",
+     answerChoices: {
+      correct: "",
+      wrong: ["", "", ""]
+
+    },
+    questionText: "",
+     answerChoices: {
+      correct: "",
+      wrong: ["", "", ""]
+
+    },
+    questionText: "",
+     answerChoices: {
+      correct: "",
+      wrong: ["", "", ""]
+
+    },
+    questionText: "",
+     answerChoices: {
+      correct: "",
+      wrong: ["", "", ""]
+
+    },
+    questionText: "",
+     answerChoices: {
+      correct: "",
+      wrong: ["", "", ""]
+
+    },
+    questionText: "",
+     answerChoices: {
+      correct: "",
+      wrong: ["", "", ""]
+
+    },
+    questionText: "",
+     answerChoices: {
+      correct: "",
+      wrong: ["", "", ""]
+
+    },
+    questionText: "",
+     answerChoices: {
+      correct: "",
+      wrong: ["", "", ""]
+
+    },
+    questionText: "",
+     answerChoices: {
+      correct: "",
+      wrong: ["", "", ""]
+
+    },
+    questionText: "",
+     answerChoices: {
+      correct: "",
+      wrong: ["", "", ""]
+
+    },
+    questionText: "",
+     answerChoices: {
+      correct: "",
+      wrong: ["", "", ""]
+
+    },
+    
+    }];
+  
+
+
+
+  
+
+
+ 
+
+Top Sellers
+5. “”
+
+	A. Solitaire
+	B. Tetris
+	C. Monopoly
+	D. Rubiks Cube
+
+
+Festive Colors
+
+1. “This hue often makes up most of the trees that are adorned for the holiday season.”
+
+	A. Green
+	B. Blue
+	C. Orange
+	D. Purple
+
+
+2. “This color has been said to represent that of Holly berries, which is believed to represent the blood of Jesus when he died on the cross.”
+
+	A. White
+	B. Red
+	C. Black
+	D. Yellow
+
+
+3. “This shade was one of the presents brought to the baby Jesus by one of the wise men and traditionally it's the color used to show the star that the wise men followed.”
+
+	A. Silver
+	B. Blue
+	C. Gold
+	D. Magenta
+
+
+  4. “This color is often associated with purity and peace in western cultures.”
+
+	A. White
+	B. Red
+	C. Green
+	D. Purple
+
+ 
+5. “This cerulean tone is often associated with Mary, the mother of Jesus.”
+
+	A. Red
+	B. Orange
+	C. Gold
+	D. Blue
+
+
+
+“We're Decking What?"
+
+1. “This ‘modern' Christmas staple is thought to have originated in 16th century Germany, where small evergreen trees were decorated with the likes of candles, apples, nuts, and berries as “Paradise trees” in church plays.”
+
+	A. Ornaments
+	B. Tree
+	C. Star
+	D. Snow
+
+
+2. “This Kwanzaa staple symbolizes the historical and traditional foundation for people to stand on and build their lives.”
+
+	A. Kinara
+	B. Mkeka
+	C. Stool
+	D. Muhindi
+
+
+3. “This Hannukah centerpiece holds nine candles that families light during the celebration: one candle on the first day, two on the second (and so on) after sundown during the eight days of Hanukkah, while reciting prayers and singing songs.”
+
+	A. Mazao
+	B. Zawadi
+	C. Menorah
+	D. Kinara
+
+
+4. “This Christmas decoration comes from the word “writhen” that was an old English word meaning “to writhe” or “to twist.”
+
+	A. Stockings
+	B. Snow Globe
+	C. Wreath
+	D. Garland
+
+
+5. “Noisemaking and this unforgettable type of show on New Years Eve is believed to have originated in ancient times, when noise and fire were thought to dispel evil spirits and bring good luck.”
+
+	A. Fireworks
+	B. Race Car
+	C. Laser Beam
+	D. Ice Skating
+
+
+
+
+ Family Carols
+
+1. “This holiday tune reminds us all to have a “very merry Christmas” and a “jolly good night”.” 
+
+	A. O’ Holy Night
+	B. Jingle Bells
+	C. Carol of the Bells
+	D. We Wish You A Merry Christmas
+
+
+2. “This famous carol recounted how someones “one true love” gave them a special gift for 12 days in a row.”
+
+	A. 12 Days of Christmas
+	B. The First Noel
+	C. Silent Night
+	D. Joy to the World
+
+
+3. “Ding, dong, ding, dong
+	That is their song
+	With joyful ring
+	All caroling
+	One seems to hear
+	Words of good cheer
+	From ev’rywhere
+	Filling the air”
+
+	A. Deck the Halls
+	B. Have Yourself a Merry Little Christmas
+	C. Carol of the Bells
+	D. Jingle Bells
+
+
+4. “This nativity anthem describes the night Christian savior, Jesus Christ, was born”
+
+	A. Silent Night
+	B. We Wish You a Merry Christmas
+	C. Frosty the Snowman
+	D. O’ Holy Night
+
+
+5. “This ballad is often sang to make sure children are on their best behavior in anticipation of a special guest on Christmas Eve.”
+
+	A.  The First Noel
+	B. Santa Claus is Coming to Town
+	C. 12 Days of Christmas
+	D. Joy to the World
+
+
+Bonus Round
+
+What is the most highly celebrated holiday in the world?
+
+	A. New Years Eve
+	B. Easter
+	C. Christmas
+	D. Independence Day
+
